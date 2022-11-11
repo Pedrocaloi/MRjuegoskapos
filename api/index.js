c939bfac6178856f1b2fcf2c225f029ced733e83
Pedro Caloiero <caloieropedro@gmail.com>
2022-11-11 12:30:08 -0300

    sadasd

--- api/index.js
+++ api/index.js
@@ -19,14 +19,14 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const { DB_PORT } = process.env;
+const { PORT } = process.env;
 const { initialGenres } = require('./src/controllers/genres.controller');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   initialGenres();
-  server.listen(DB_PORT, () => {
-    console.log(`% Server listening at ${DB_PORT}`); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`% Server listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
  
